f0c571151fcd43e49c81d8aa69647fb7f4a8a79b
Gabriel Oliveira Rodrigues <gabrielrrodrigues133@gmail.com>
2023-04-19 12:15:11 -0300

    Estudos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,101 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // Concatenação
+        /*
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        Operadores unários:
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+        */
+
+        // Converte negativo em positivo
+        /*
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        Incremento:
+        int numero = 5;
+        x repetição
+
+        numero = numero + 1;
+        
+        System.out.println( ++ numero );
+
+        System.out.println(numero);
+        */
+
+        //Nengando expressões booleanas:
+        /* boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+        */
+
+        // Operador Tenário
+        /*
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+         */
+
+         // Operadores relacionais:
+
+         //Comprarando objetos, usando equals:
+         /*
+          String nomeUm = "GLEYSON";
+         String nomeDois = new String("GLEYSON");
+
+         System.out.println(nomeUm.equals(nomeDois));
+
+
+         int numero1 = 1;
+         int numero2 = 2;
+
+         boolean simNao = numero1 == numero2;
+
+         if(numero1 < numero2) {
+            System.out.println("A nossa condição é verdadeira");
+         }
+
+
+         System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+         simNao = numero1 != numero2;
+
+         System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+         simNao = numero1 > numero2;
+
+         System.out.println("numeroUm é maior que numeroDois? " + simNao);
+          */
+
+          // Operadores lógicos:
+          /*
+          boolean condicao1 = true;
+          boolean condicao2 = false;
+
+          if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+          }
+
+          if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+          }
+
+          System.out.println("Fim");
+           */
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        // Classes String 
+
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        // O short por mais que o número seja 1(curto), pode ser que esse valor seja longo e o short não comportaria esse valor
+        short numeroCurto2 = (short) numeroNormal;
+    
+    
+        int numero = 5;
+        
+        numero = 10;
+
+        // O nome da váriavel constante, por conveção deve ser em caixa alta
+        final double NUMERO_PI = 3.14;
     }
 }
